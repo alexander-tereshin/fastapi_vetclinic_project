f722f32d5980625a96ea6c7a5301c663408a2a69
Alexander <alexandr.tereshin@outlook.com>
2023-11-08 10:56:27 +0300

    main.py update

--- main.py
+++ main.py
@@ -51,16 +51,24 @@ def get_db_dogs() -> Dict[int, Dog]:
 @app.get("/", summary="Root", operation_id='root__get')
 async def root():
     """
-    :return:
+    Root endpoint that returns a greeting message.
+
+    Returns:
+        dict: A dictionary with a greeting message.
     """
     return {"message": "Hello, User!"}
 
 
-@app.post("/add-post", summary="Get Post", operation_id='get_post_post_post', response_model=Timestamp)
+@app.post("/post", summary="Get Post", operation_id='get_post_post_post', response_model=Timestamp)
 async def add_item(db=Depends(get_db_post)) -> Timestamp:
     """
-    :param db:
-    :return:
+    Create and return a new Timestamp.
+
+    Args:
+        db (List[Timestamp]): The list of Timestamp items.
+
+    Returns:
+        Timestamp: The newly created Timestamp.
     """
     max_id = max([i.id for i in db])
     dt = datetime.now()
@@ -73,9 +81,14 @@ async def add_item(db=Depends(get_db_post)) -> Timestamp:
 @app.get('/dog', response_model=List[Dog], summary='Get Dogs', operation_id='get_dogs_dog_get')
 async def get_dogs(kind: Optional[DogType] = None, db=Depends(get_db_dogs)) -> list[Dog]:
     """
-    :param kind:
-    :param db:
-    :return:
+    Get a list of Dog items filtered by kind.
+
+    Args:
+        kind (Optional[DogType]): The kind of Dog to filter by.
+        db (Dict[int, Dog]): The database of Dog items.
+
+    Returns:
+        list[Dog]: A list of Dog items.
     """
     if kind:
         return list(filter(lambda x: x.kind == kind, db.values()))
@@ -85,9 +98,14 @@ async def get_dogs(kind: Optional[DogType] = None, db=Depends(get_db_dogs)) -> l
 @app.post('/dog', response_model=Dog, summary='Create Dog', operation_id='create_dog_dog_post')
 async def create_dogs(dog: Dog, db=Depends(get_db_dogs)) -> Dog:
     """
-    :param dog:
-    :param db:
-    :return:
+    Create and return a new Dog item.
+
+    Args:
+        dog (Dog): The Dog item to create.
+        db (Dict[int, Dog]): The database of Dog items.
+
+    Returns:
+        Dog: The newly created Dog item.
     """
     if db.get(dog.pk):
         raise HTTPException(status_code=422, detail="pk already exists")
@@ -98,9 +116,14 @@ async def create_dogs(dog: Dog, db=Depends(get_db_dogs)) -> Dog:
 @app.get('/dog/{pk}', response_model=Dog, summary='Get Dog By Pk', operation_id='get_dog_by_pk_dog__pk__get')
 async def get_dog_by_pk(pk: int, db=Depends(get_db_dogs)) -> Dog:
     """
-    :param pk:
-    :param db:
-    :return:
+    Get a Dog item by its primary key (pk).
+
+    Args:
+        pk (int): The primary key of the Dog item to retrieve.
+        db (Dict[int, Dog]): The database of Dog items.
+
+    Returns:
+        Dog: The retrieved Dog item.
     """
     if not db.get(pk):
         raise HTTPException(status_code=422, detail="pk does not exists")
@@ -110,10 +133,15 @@ async def get_dog_by_pk(pk: int, db=Depends(get_db_dogs)) -> Dog:
 @app.patch('/dog/{pk}', response_model=Dog, summary='Update Dog', operation_id='update_dog_dog__pk__patch')
 async def update_dog_by_pk(pk: int, dog: Dog, db=Depends(get_db_dogs)) -> Dog:
     """
-    :param pk:
-    :param dog:
-    :param db:
-    :return:
+    Update a Dog item by its primary key (pk).
+
+    Args:
+        pk (int): The primary key of the Dog item to update.
+        dog (Dog): The new Dog item data.
+        db (Dict[int, Dog]): The database of Dog items.
+
+    Returns:
+        Dog: The updated Dog item.
     """
     if not db.get(pk):
         raise HTTPException(status_code=422, detail="pk does not exists")
